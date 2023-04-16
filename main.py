b121683b2d3f06fab153c069660cb44bb3c8ae36
Harjot Singh <HarjjotSinghh@gmail.com>
2023-04-16 18:43:21 +0530

    Extracted Database Information
    API used: Device-Specs

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import requests_async
 from sanic import Sanic
 from sanic.response import text, html, json
 import json
 import requests, asyncio, aiohttp
 
+
 config_file = open("config.json")
 config = json.load(config_file)
+
 
 app = Sanic(name="InnovaTech")
 
@@ -23,18 +24,27 @@ async def get_url(url, headers):
 
 @app.get("/")
 async def welcome(request):
-    data = {}
+    data : dict = {}
     # url = "https://mobile-phone-specs-database.p.rapidapi.com/gsm/get-models-by-brandname/Samsung"
     # headers = {
     #     "X-RapidAPI-Key": config["RAPID_API_KEY"],
     #     "X-RapidAPI-Host": "mobile-phone-specs-database.p.rapidapi.com"
     # }
-    url = "https://api.device-specs.io/api/smartphones?pagination[page]=2&pagination[pageSize]=35&populate=*"
-    headers = {
-        "Authorization": f"Bearer {config['DEVICE_SPECS_API_TOKEN']}"
-    }
-    response = await get_url(url=url, headers=headers)
-    return text(str(response))
+    page_no = 1
+    for i in range(1, 76):
+        url = f"https://api.device-specs.io/api/smartphones?pagination[page]={i}&pagination[pageSize]=35&populate=*"
+        headers = {
+            "Authorization": f"Bearer {config['DEVICE_SPECS_API_TOKEN']}"
+        }
+        response = await get_url(url=url, headers=headers)
+        page_no += 1
+        response = json.loads(response)
+        with open("smartphones_data.json", "a") as f:
+            json.dump(response, f, indent=4)
+
+        # data.update(response)
+
+    return text("Success")
 
 
 if __name__ == '__main__':
