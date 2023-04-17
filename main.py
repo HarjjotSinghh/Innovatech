66241afb0cccde3352350c3f300846d18f52adf3
Harjot Singh <HarjjotSinghh@gmail.com>
2023-04-17 19:29:13 +0530

    Connected To MongoDB

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from pymongo import InsertOne
 from sanic import Sanic
 from sanic.response import text, html, json
 import json
 import requests, asyncio, aiohttp
+import motor.motor_asyncio, pymongo
 
 
 config_file = open("config.json")
@@ -9,6 +11,9 @@ config = json.load(config_file)
 
 
 app = Sanic(name="InnovaTech")
+db_url = f"mongodb+srv://{config['MONGODB_USERNAME']}:{config['MONGODB_PASSWORD']}@cluster0.jd36ygh.mongodb.net/?retryWrites=true&w=majority"
+db_client = motor.motor_asyncio.AsyncIOMotorClient(db_url)
+db = db_client["Smartphones"]["Smartphones"]
 
 
 async def fetch(session, url, headers):
@@ -24,25 +29,29 @@ async def get_url(url, headers):
 
 @app.get("/")
 async def welcome(request):
-    data : dict = {}
+    # data : dict = {}
     # url = "https://mobile-phone-specs-database.p.rapidapi.com/gsm/get-models-by-brandname/Samsung"
     # headers = {
     #     "X-RapidAPI-Key": config["RAPID_API_KEY"],
     #     "X-RapidAPI-Host": "mobile-phone-specs-database.p.rapidapi.com"
     # }
-    page_no = 1
-    for i in range(1, 76):
-        url = f"https://api.device-specs.io/api/smartphones?pagination[page]={i}&pagination[pageSize]=35&populate=*"
-        headers = {
-            "Authorization": f"Bearer {config['DEVICE_SPECS_API_TOKEN']}"
-        }
-        response = await get_url(url=url, headers=headers)
-        page_no += 1
-        response = json.loads(response)
-        with open("smartphones_data.json", "a") as f:
-            json.dump(response, f, indent=4)
+    # page_no = 1
+    # for i in range(1, 76):
+    #     url = f"https://api.device-specs.io/api/smartphones?pagination[page]={i}&pagination[pageSize]=35&populate=*"
+    #     headers = {
+    #         "Authorization": f"Bearer {config['DEVICE_SPECS_API_TOKEN']}"
+    #     }
+    #     response = await get_url(url=url, headers=headers)
+    #     page_no += 1
+    #     response = json.loads(response)
+    #     with open("smartphones_data.json", "a") as f:
+    #         json.dump(response, f, indent=4)
 
         # data.update(response)
+
+    with open("smartphones_data.json") as f:
+        file_data = json.load(f)
+        await db.insert_one(file_data)
 
     return text("Success")
 
